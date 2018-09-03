fa88a517ec677b1bae79c4a29591accf4be63c07
Weintraubm <weintraubm@dewv.edu>
2018-09-03 16:55:29 +0000

    Updates via sync script

--- wizardsDuel.js
+++ wizardsDuel.js
@@ -1,7 +1,6 @@
 import { game, Sprite } from "./sgc/sgc.js";
 
 game.setBackground("floor.png");
-
 class PlayerWizard extends Sprite {
     constructor() {
         super();
@@ -21,18 +20,51 @@ class PlayerWizard extends Sprite {
         this.defineAnimation("down", 6, 8);
 
         this.speedWhenWalking = 100;
+
+
     }
     handleDownArrowKey() {
 
-    this.playAnimation("down");
+        this.playAnimation("down");
 
-    this.speed = this.speedWhenWalking;
+        this.speed = this.speedWhenWalking;
 
-    this.angle = 270;
+        this.angle = 270;
+
+    }
+    handlSpaceBar(){
+        let spell;
+        
+        spell.x = this.x; // this sets the position of the spell object equal to
+
+        spell.x = this.y; // the position of any object created from the PlayerWizard class
+        
+        spell.name = "A spell cast by Marcus";
+        
+        spell.setImage("marcusSpellSheet.png");
+        
+        spell.angle = 0;
+        
+    }
+        
+    handleGameLoop() {
+        this.y = Math.max(0, this.y);
+
+        this.y = Math.min(552, this.y);
+    }
 
 }
-
-}
+        
 
 let marcus = new PlayerWizard();
 
+class Spell extends Sprite {
+    constructor() {
+        super();
+        this.speed = 200;
+        this.height = 48;
+        this.width = 48;
+        this.defineAnimation("magic", 0, 7);
+        this.playAnimation("magic", true);
+    }
+}
